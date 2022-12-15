000d1907a02bfeaba7f4e707c77dfd76a133e2ab
Laura-dias388 <ldiasvitaltoledo8@gmail.com>
2022-12-15 19:06:37 -0300

    Requisito 7, concluído

--- src/routes/RoutersUsers.ts
+++ src/routes/RoutersUsers.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
 
 import controllerUsers from '../controllers/ControllersUsers';
+import { 
+  validateUsername,
+  validateVocation,
+  validateLevel,
+  validatePassword,
+} from '../middlewares/validateUsers';
 
 const router = Router();
 
-router.post('/', controllerUsers.createUsers);
+router.post(
+  '/',
+  validateUsername,
+  validateVocation, 
+  validateLevel, 
+  validatePassword,
+  controllerUsers.createUsers,
+);
 
 export default router;
--- src/types/index.ts
+++ src/types/index.ts
@@ -17,11 +17,3 @@ export type Orders = {
   productIds: number;
   userId: number;
 };
-
-/* CREATE TABLE Trybesmith.products (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  name TEXT NOT NULL,
-  amount TEXT NOT NULL,
-  order_id INTEGER,
-  FOREIGN KEY (order_id) REFERENCES Trybesmith.orders (id)
-); */
